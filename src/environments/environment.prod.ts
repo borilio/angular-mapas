9b7993339d18000964f3d280b1123fe2d3ea8150
borilio <exprimetuandroid@gmail.com>
2021-10-22 14:50:54 +0200

    Instalación de las librerías de MapBox en el proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYm9yaWxpbzEzIiwiYSI6ImNrdjB5dm93ODBidXQydm92YThreWdiYzcifQ.cTzU1NwIS0UeEQjLFvX2WQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYm9yaWxpbzEzIiwiYSI6ImNrdjB5dm93ODBidXQydm92YThreWdiYzcifQ.cTzU1NwIS0UeEQjLFvX2WQ'
 };
 
 /*

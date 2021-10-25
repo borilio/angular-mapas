632d465e2bb605e27f50483a581e539c4071d58f
borilio <exprimetuandroid@gmail.com>
2021-10-25 14:25:06 +0200

    Maquetación de HTML y CSS del componente zoom-range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
@@ -10,6 +12,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-4.554049212537303, 36.73842272092004],
+      zoom: 15
+    });
   }
 
 }
